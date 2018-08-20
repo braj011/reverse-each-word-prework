e07a8f6f21d888a7b5497d85c2adf8ee306ab0fb
braj011 <bhagyaraj01@gmail.com>
2018-08-20 18:51:24 +0000

    automatically backed up by learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,15 @@
+def reverse_each_word(string)
+  
+end 
+
+=begin 
+
+hello 
+
+describe '#reverse_each_word' do
+  let(:sentence1) { "Hello there, and how are you?" }
+  it 'reverses all the words in a string without reversing the order of the words' do
+    expect(reverse_each_word(sentence1)).to eq("olleH ,ereht dna woh era ?uoy")
+  end
+
+=end 
